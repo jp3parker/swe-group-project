9279979b4f24bea17860fff33c3b9f45cec13b0a
jp3parker <jp3parker@gmail.com>
2022-11-22 18:31:26 -0600

    added database stuff - trying to push to fly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 """docstring"""
 import os
 from PIL import Image
+import requests
+from io import BytesIO
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask, render_template, redirect, url_for, request
 from google_images_search import GoogleImagesSearch
@@ -19,50 +21,56 @@ GCS_CX=os.getenv("GCS_CX")
 gis = GoogleImagesSearch(GCS_DEVELOPER_KEY, GCS_CX)
 app = Flask(__name__)
 image_search_results = ["", "", ""] # placeholders
-# app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-# app.secret_key = os.getenv("SECRET_KEY")
-# db = SQLAlchemy(app)
-# login_manager = LoginManager()
-# login_manager.init_app(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.secret_key = os.getenv("SECRET_KEY")
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 # ascii characters used to build the output text
 ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", ".", " "]
 
-
-# class Person(UserMixin, db.Model):
-#     """Person object is what is stored in the table that holds user data"""
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-
-#     def __init__(self, name):
-#         self.username = name
+_search_params = {
+    'q': '...',
+    'num': 3,
+    'fileType': 'jpg|png',
+}
 
 
-# class Picture(db.Model):
-#     """Picture object is what is stored in the table that holds picture data"""
+class Person(UserMixin, db.Model):
+    """Person object is what is stored in the table that holds user data"""
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), nullable=False)
-#     picture = db.Column(db.String(10000), nullable=False)
-#     width = db.Column(db.Integer, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
 
-#     def __init__(self, id, username, picture, width):
-#         self.id = id
-#         self.username = username
-#         self.picture = picture
-#         self.width = width
+    def __init__(self, name):
+        self.username = name
 
 
-# with app.app_context():
-#     db.create_all()
+class Picture(db.Model):
+    """Picture object is what is stored in the table that holds picture data"""
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    picture = db.Column(db.String(10000), nullable=False)
+    width = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, username, picture, width):
+        self.username = username
+        self.picture = picture
+        self.width = width
 
 
-# @login_manager.user_loader
-# def load_user(user_id):
-#     """returns user object with user_id as their id"""
-#     return Person.query.get(int(user_id))
+with app.app_context():
+    db.create_all()
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    """returns user object with user_id as their id"""
+    return Person.query.get(int(user_id))
+
+
+@app.route("/", methods=["GET", "POST"])
 @app.route("/login_page", methods=["GET", "POST"])
 def login_page():
     """loads login page"""
@@ -71,8 +79,17 @@ def login_page():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """nothing yet - just calls login_page"""
-    return "redirect(login_page())"
+    if request.method == "POST" and "username" in request.form:  # logging in
+        print("valid")
+        name = request.form["username"]
+        user = Person.query.filter_by(username=name).first()
+        if user:
+            login_user(user)
+            return redirect(url_for("index"))
+        else:
+            return redirect(url_for("signup_page"))
+    else:
+        return redirect(url_for("signup_page"))
 
 
 @app.route("/signup_page", methods=["GET", "POST"])
@@ -83,31 +100,47 @@ def signup_page():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
-    """nothing yet - just calls login_page"""
-    return "redirect(signup_page())"
-
-
-@app.route("/", methods=["GET", "POST"])
-@app.route("/home", methods=["GET", "POST"])
-def index(show_searched_images =False):
-    print("show_searched_images = ", show_searched_images)
-    if show_searched_images:
-        print("trying to print images")
-        print(image_search_results[0])
-        return render_template('index.html', image_search_results=image_search_results)
+    if request.method == "POST" and "username" in request.form:  # signing up
+        print("trying to sign up.")
+        name = request.form["username"]
+        user = Person.query.filter_by(username=name).first()
+        if user:
+            print("already have an account")
+            return redirect(url_for("login_page"))
+        else:
+            print("trying to insert new person")
+            new_entry = Person(name)
+            db.session.add(new_entry)
+            db.session.commit()
+            return redirect(url_for("login_page"))
     else:
-        print('nothing here - no images')
-        return render_template('index.html', image_search_results=[])
+        print("something went wrong with signup.")
+        return redirect(url_for("signup_page"))
+
+
+
+@app.route("/home", methods=["GET", "POST"])
+@login_required
+def index(show_searched_images =False):
+
+    pictures = Picture.query.filter_by(username=current_user.username)
+    for pic in pictures:
+        pic.picture.replace("\n", "<br>")
+
+    if show_searched_images:
+        return render_template('index.html', image_search_results=image_search_results, pictures=pictures)
+    else:
+        return render_template('index.html', image_search_results=[], pictures=pictures)
 
 
 @app.route("/imageSearch", methods=["GET", "POST"])
 def imageSearch():
-    print("THIS IS THE METHOD USED ==== ", request.method)
     if (
         request.method == "POST"
         and "searchWord" in request.form
     ):
-        gis.search({'q': str(request.form['searchWord']), 'num': 3})
+        _search_params['q'] = str(request.form['searchWord'])
+        gis.search(search_params=_search_params)
         results = gis.results()
         print(image_search_results[0])
         print(image_search_results[1])
@@ -121,8 +154,41 @@ def imageSearch():
         return index(True)
     else:
         # flash("We could not process your comment.")
-        print("fuck no")
         return index(False)
+    
+
+@app.route('/ascifySearchedImage', methods=["GET", "POST"])
+@login_required
+def ChooseSearchedImage():
+    if request.method == "POST":
+        url = image_search_results[0] if request.form["image"] == "image1" else \
+              image_search_results[1] if request.form["image"] == "image2" else \
+              image_search_results[2]
+        response = requests.get(url)
+        image = Image.open(BytesIO(response.content))
+        new_width = image.width
+        while (image.height * image.width > 10000):
+            new_width = int(new_width * 0.9)
+            image = resize_image(image, new_width)
+
+        new_image_data = pixels_to_ascii(grayify(image))
+        pixel_count = len(new_image_data)
+        ascii_image = "\n".join([new_image_data[index:(index+new_width)] \
+            for index in range(0, pixel_count, new_width)])
+        print(ascii_image)
+
+        new_picture = Picture(current_user.username, ascii_image, new_width)
+        db.session.add(new_picture)
+        db.session.commit()
+
+        # save result to "ascii_image.txt"
+        with open("ascii_image.txt", "w") as f:
+            f.write(ascii_image)
+    else:
+        print("it failed")
+    print("got here 2!!!!!!!!!!!!!!!!!!")
+    return redirect(url_for("index"))
+
 
 @app.route('/fileUpload', methods=["GET", "POST"])
 def fileUpload():
@@ -139,13 +205,9 @@ def fileUpload():
             return index(False)
 
         new_width = image.width
-        print("width = ", image.width)
-        print("height = ", image.height)
         while (image.height * image.width > 10000):
             new_width = int(new_width * 0.9)
             image = resize_image(image, new_width)
-            print("width = ", image.width)
-            print("height = ", image.height)
 
         new_image_data = pixels_to_ascii(grayify(image))
         pixel_count = len(new_image_data)
@@ -181,8 +243,6 @@ def grayify(image):
 def pixels_to_ascii(image):
     """converts pixels of image to ascii format"""
     pixels = image.getdata()
-    for pixel in range(len(pixels)):
-        print(pixels[pixel])
     characters = "".join([ASCII_CHARS[pixel//22] for pixel in pixels])
     return characters
 
