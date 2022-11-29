2c95d0e5d1093c559166ac725228f6556ee88506
jp3parker <jp3parker@gmail.com>
2022-11-29 15:35:02 -0600

    removed some internal server errors and reformatted with black and pylint

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 """docstring"""
 import os
+from io import BytesIO
 from PIL import Image
 import requests
-from io import BytesIO
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask, render_template, redirect, url_for, request
 from flask_login import (
     LoginManager,
     UserMixin,
     current_user,
-    login_required,
     login_user,
 )
 from flask_sqlalchemy import SQLAlchemy
+import psycopg2
 
 load_dotenv(find_dotenv())
-GCS_DEVELOPER_KEY=os.getenv("GCS_DEVELOPER_KEY")
-GCS_CX=os.getenv("GCS_CX")
+GCS_DEVELOPER_KEY = os.getenv("GCS_DEVELOPER_KEY")
+GCS_CX = os.getenv("GCS_CX")
 app = Flask(__name__)
-image_search_results = ["", "", ""] # placeholders
+image_search_results = ["", "", ""]  # placeholders
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.secret_key = os.getenv("SECRET_KEY")
 db = SQLAlchemy(app)
@@ -27,13 +27,13 @@ login_manager.init_app(app)
 # ascii characters used to build the output text
 ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", ".", " "]
 
-GOOGLE_CUSTOM_SEARCH_URL="https://www.googleapis.com/customsearch/v1"
+GOOGLE_CUSTOM_SEARCH_URL = "https://www.googleapis.com/customsearch/v1"
 search_params = {
-    'key': GCS_DEVELOPER_KEY,
-    'cx': GCS_CX,
-    'q': '...', # place holder
-    'searchType': 'image',
-    'num': 3
+    "key": GCS_DEVELOPER_KEY,
+    "cx": GCS_CX,
+    "q": "...",  # place holder
+    "searchType": "image",
+    "num": 3,
 }
 
 
@@ -80,14 +80,17 @@ def login_page():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    """called when user hits submit on login page"""
     if request.method == "POST" and "username" in request.form:  # logging in
         name = request.form["username"]
-        user = Person.query.filter_by(username=name).first()
+        try:
+            user = Person.query.filter_by(username=name).first()
+        except psycopg2.OperationalError:
+            user = Person.query.filter_by(username=name).first()
         if user:
             login_user(user)
             return redirect(url_for("index"))
-        else:
-            return redirect(url_for("signup_page"))
+        return redirect(url_for("signup_page"))
     else:
         return redirect(url_for("signup_page"))
 
@@ -100,9 +103,13 @@ def signup_page():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+    """called when user hits submit on signup page"""
     if request.method == "POST" and "username" in request.form:  # signing up
         name = request.form["username"]
-        user = Person.query.filter_by(username=name).first()
+        try:
+            user = Person.query.filter_by(username=name).first()
+        except psycopg2.OperationalError:
+            user = Person.query.filter_by(username=name).first()
         if user:
             return redirect(url_for("login_page"))
         else:
@@ -114,60 +121,80 @@ def signup():
         return redirect(url_for("signup_page"))
 
 
-
 @app.route("/home", methods=["GET", "POST"])
-@login_required
-def index(show_searched_images =False):
-    pictures = Picture.query.filter_by(username=current_user.username)
+def index(show_searched_images=False):
+    """this is the home page - it is called when an update to the page is
+    required (user adds another image) or when user has successful login"""
+    if not current_user.is_authenticated:
+        return redirect(url_for("login_page"))
+
+    try:
+        pictures = Picture.query.filter_by(username=current_user.username)
+    except psycopg2.OperationalError:
+        pictures = Picture.query.filter_by(username=current_user.username)
 
     for pic in pictures:
         pic.picture.replace("\n", "<br>")
 
     if show_searched_images:
-        return render_template('index.html', image_search_results=image_search_results, pictures=pictures)
+        return render_template(
+            "index.html", image_search_results=image_search_results, pictures=pictures
+        )
     else:
-        return render_template('index.html', image_search_results=[], pictures=pictures)
+        return render_template("index.html", image_search_results=[], pictures=pictures)
 
 
 @app.route("/imageSearch", methods=["GET", "POST"])
-def imageSearch():
-    if (
-        request.method == "POST"
-        and "searchWord" in request.form
-    ):
-        search_params['q'] = str(request.form['searchWord'])
+def image_search():
+    """this is called when user searches for an image - uses the google images api"""
+    if not current_user.is_authenticated:
+        return redirect(url_for("login_page"))
+
+    if request.method == "POST" and "searchWord" in request.form:
+        search_params["q"] = str(request.form["searchWord"])
         response = requests.get(
-            GOOGLE_CUSTOM_SEARCH_URL,
-            params = search_params
+            GOOGLE_CUSTOM_SEARCH_URL, params=search_params, timeout=10
         ).json()
-        image_search_results[0] = response["items"][0]['link']
-        image_search_results[1] = response["items"][1]['link']
-        image_search_results[2] = response["items"][2]['link']
+        image_search_results[0] = response["items"][0]["link"]
+        image_search_results[1] = response["items"][1]["link"]
+        image_search_results[2] = response["items"][2]["link"]
         return index(True)
     else:
         # flash("We could not process your comment.")
         return index(False)
-    
 
-@app.route('/ascifySearchedImage', methods=["GET", "POST"])
-@login_required
-def ChooseSearchedImage():
+
+@app.route("/ascifySearchedImage", methods=["GET", "POST"])
+def choose_searched_image():
+    """this function is called when a user selects a searched image that
+    they want to display as ascii art"""
+    if not current_user.is_authenticated:
+        return redirect(url_for("login_page"))
+
     if request.method == "POST":
-        url = image_search_results[0] if request.form["image"] == "image1" else \
-              image_search_results[1] if request.form["image"] == "image2" else \
-              image_search_results[2]
-        response = requests.get(url)
+        url = (
+            image_search_results[0]
+            if request.form["image"] == "image1"
+            else image_search_results[1]
+            if request.form["image"] == "image2"
+            else image_search_results[2]
+        )
+        response = requests.get(url, timeout=10)
         image = Image.open(BytesIO(response.content))
-        image = image.resize((image.width, int(3/4*image.height)))
+        image = image.resize((image.width, int(3 / 4 * image.height)))
         new_width = image.width
-        while (image.height * image.width > 20000):
+        while image.height * image.width > 20000:
             new_width = int(new_width * 0.9)
             image = resize_image(image, new_width)
 
         new_image_data = pixels_to_ascii(grayify(image))
         pixel_count = len(new_image_data)
-        ascii_image = "\n".join([new_image_data[index:(index+new_width)] \
-            for index in range(0, pixel_count, new_width)])
+        ascii_image = "\n".join(
+            [
+                new_image_data[index : (index + new_width)]
+                for index in range(0, pixel_count, new_width)
+            ]
+        )
 
         new_picture = Picture(current_user.username, ascii_image, new_width)
         db.session.add(new_picture)
@@ -180,24 +207,32 @@ def ChooseSearchedImage():
     return redirect(url_for("index"))
 
 
-@app.route('/fileUpload', methods=["GET", "POST"])
-def fileUpload():
-    if request.method == "POST" \
-    and request.files:
+@app.route("/fileUpload", methods=["GET", "POST"])
+def file_upload():
+    """called when user sends in an image through a file
+    that they want to display as ascii art"""
+    if not current_user.is_authenticated:
+        return redirect(url_for("login_page"))
+
+    if request.method == "POST" and request.files:
         try:
             image = Image.open(request.files["file"])
-        except:
+        except Exception:
             return index(False)
-        image = image.resize((image.width, int(3/4*image.height)))
+        image = image.resize((image.width, int(3 / 4 * image.height)))
         new_width = image.width
-        while (image.height * image.width > 10000):
+        while image.height * image.width > 10000:
             new_width = int(new_width * 0.9)
             image = resize_image(image, new_width)
 
         new_image_data = pixels_to_ascii(grayify(image))
         pixel_count = len(new_image_data)
-        ascii_image = "\n".join([new_image_data[index:(index+new_width)] \
-            for index in range(0, pixel_count, new_width)])
+        ascii_image = "\n".join(
+            [
+                new_image_data[index : (index + new_width)]
+                for index in range(0, pixel_count, new_width)
+            ]
+        )
 
         new_picture = Picture(current_user.username, ascii_image, new_width)
         db.session.add(new_picture)
@@ -214,10 +249,11 @@ def fileUpload():
 def resize_image(image, new_width):
     """resize image to desired width"""
     width, height = image.size
-    ratio = height/width
+    ratio = height / width
     new_height = int(new_width * ratio)
     resized_image = image.resize((new_width, new_height))
     return resized_image
+
 
 # convert each pixel to grayscale
 def grayify(image):
@@ -230,7 +266,7 @@ def grayify(image):
 def pixels_to_ascii(image):
     """converts pixels of image to ascii format"""
     pixels = image.getdata()
-    characters = "".join([ASCII_CHARS[pixel//22] for pixel in pixels])
+    characters = "".join([ASCII_CHARS[pixel // 22] for pixel in pixels])
     return characters
 
 
